e06e539b236d942cada44a70c0a229a10bad8fb4
Greenfoot5 <greenfoot89@gmail.com>
2020-07-23 17:48:46 +0100

    Added a main scene for things like player customisations, and what duel to select.

---
+++ Assets/MainManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+    public Player player;
+
+    public void Awake ()
+    {
+        SceneManager.LoadScene("PlayerScene", LoadSceneMode.Additive);
+    }
+
+    public void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    public void GoToDuel()
+    {
+        SceneManager.LoadScene("Duel", LoadSceneMode.Single);
+    }
+}
