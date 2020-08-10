aa292297b96761ab36d01507fad9092792e35ff1
Greenfoot5 <greenfoot89@gmail.com>
2020-08-10 23:47:41 +0100

    Got the pages to swipe. Images next time perhaps.

--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -3,66 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-// TODO - COMMENT!!!!
 public class PageManager : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-    [SerializeField]
+    // The location of the current panel.
     private Vector3 panelLocation;
+    // The amount of the screen you need to swipe to switch menus.
     public float percentThreshold = 0.2f;
+    // The time to transition to the next screen.
     public float easing = 0.5f;
-    public int totalPages = 2;
-    [SerializeField]
-    private int currentPage = 2;
+    // The total number of pages cycling through
+    public int totalPages = 1;
+    // The index of the page, index starts at 1.
+    public int currentPage = 1;
 
-    public RectTransform canvas;
-
-    // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
+        // Sets the panelLocation to the current location
         panelLocation = transform.position;
     }
-    
-    public void OnDrag(PointerEventData data){
-        // The amount we've moved
-        float difference = (data.pressPosition.x - data.position.x);
-        // Move the current panel
-        transform.position = panelLocation - new Vector3(difference / 5, 0, 0);
+
+    // Used when the finger is present on the screen
+    public void OnDrag(PointerEventData data)
+    {
+        // Get's the distance the finger moved.
+        float difference = data.pressPosition.x - data.position.x;
+
+        // Move the panel according to how much the finger moved.
+        transform.position = panelLocation - new Vector3(difference, 0, 0);
     }
 
-    public void OnEndDrag(PointerEventData data){
-        // Percentage of the screen we've swiped
-        float width = canvas.rect.width;
-        Debug.Log(width);
-        float percentage = (data.pressPosition.x - data.position.x) / (width);
-
-        // Check if we've moved enough
+    // Used when the finger is lifted from the screen
+    public void OnEndDrag(PointerEventData data)
+    {
+        // Get the % of the screen the finger moved across
+        float percentage = (data.pressPosition.x - data.position.x) / Screen.width;
+        // Check if we're above the % threshold so we can move to the next menu
         if(Mathf.Abs(percentage) >= percentThreshold)
         {
-            // Create a new location, and set it to the idle state.
+            // Set the new location to the current location
+            // We use this if there's no next page in the target direction.
             Vector3 newLocation = panelLocation;
 
-            // If we've moved to the right
-            if(percentage > 0)
+            // Check which way we moved, and that there's another page in that direction
+            if(percentage > 0 && currentPage < totalPages)
             {
-                newLocation += new Vector3(-width, 0, 0);
+                // Increment the page number
+                currentPage++;
+                // Set the new location to the next page's location
+                newLocation += new Vector3(-Screen.width, 0, 0);
             }
-            else if (percentage <0) 
+            else if(percentage < 0 && currentPage > 1)
             {
-                newLocation += new Vector3(width, 0, 0);   
+                currentPage--;
+                newLocation += new Vector3(Screen.width, 0, 0);
             }
-            transform.position = newLocation;
+
+            StartCoroutine(SmoothMove(transform.position, newLocation, easing));
             panelLocation = newLocation;
         }
         else
         {
-            transform.position = panelLocation;
+            StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
         }
     }
 
-    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds){
+    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds)
+    {
+        // Start the animation at time = 0
         float t = 0f;
-        while(t <= 1.0){
+        // Loop through the full animation.
+        while(t <= 1f)
+        {
+            // Increases t with time
             t += Time.deltaTime / seconds;
-            transform.position = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0f, 1f, t));
+
+            // Get the progress through the lerp. We use a smoothstep to make it a more natural animation.
+            float smoothStep = Mathf.SmoothStep(0f, 1f, t);
+
+            // Actually move the panel.
+            transform.position = Vector3.Lerp(startpos, endpos, smoothStep);
+
+            // Wait one frame
             yield return null;
         }
     }
